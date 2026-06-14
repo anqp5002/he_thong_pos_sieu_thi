174035c418403df03114e1fc25a69d8e37aab964
Ho Van Duc <ducho6699@gmail.com>
2026-06-14 09:15:00 +0700

    feat: add loading spinner overlay and centralized error handler

---
+++ src/HeThongPOS.WPF/Controls/LoadingSpinner.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace HeThongPOS.WPF.Controls;
+
+public partial class LoadingSpinner : UserControl
+{
+    public LoadingSpinner()
+    {
+        InitializeComponent();
+    }
+}
